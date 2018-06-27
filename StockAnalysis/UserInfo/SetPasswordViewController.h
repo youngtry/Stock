c1dac87a7056571172e8731da341b4cb594c56b6
try <youngtry@163.com>
2018-06-27 18:21:03 +0800

    no message

---
+++ StockAnalysis/UserInfo/SetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetPasswordViewController.h
+//  StockAnalysis
+//
+//  Created by try on 2018/6/27.
+//  Copyright © 2018年 try. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetPasswordViewController : UIViewController
+
+@end
--- StockAnalysis/UserInfo/SetPasswordViewController.m
+++ StockAnalysis/UserInfo/SetPasswordViewController.m
@@ -1,23 +1,23 @@
 //
-//  SafeViewController.m
+//  SetPasswordViewController.m
 //  StockAnalysis
 //
-//  Created by try on 2018/6/21.
+//  Created by try on 2018/6/27.
 //  Copyright © 2018年 try. All rights reserved.
 //
 
-#import "SafeViewController.h"
+#import "SetPasswordViewController.h"
 
-@interface SafeViewController ()
+@interface SetPasswordViewController ()
 
 @end
 
-@implementation SafeViewController
+@implementation SetPasswordViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.title = @"安全中心";
+    self.title = @"设置资金密码";
 }
 
 - (void)didReceiveMemoryWarning {
